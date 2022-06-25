a3e14849478d7b7a2b17929b34d55de43f401782
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-25 14:33:14 -0400

    first commit

---
+++ client/webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+// https://michaelceber.medium.com/how-to-setup-and-use-css-modules-in-react-with-webpack-7f512b946ae0
+
+module.exports = (env, options) => {
+  const prod = options.mode === 'production';
+
+  const devOptions = prod ? null : { devtool: 'eval-source-map' };
+  return {
+    ...devOptions,
+    entry: './src/index.tsx',
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, './build'),
+      clean: true,
+      publicPath: '/',
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            { loader: 'style-loader' },
+            // Translates CSS into CommonJS
+            // Compiles Sass to CSS
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  // getLocalIdent: getCSSModuleLocalIdent,
+                  localIdentName: '[name]_[local]_[hash:base64]',
+                },
+                importLoaders: 2,
+                sourceMap: true,
+              },
+            },
+            { loader: 'sass-loader' },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|jp2|webp)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      // @ts-ignore
+      new ESLintPlugin({}),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        inject: true,
+      }),
+    ],
+    devServer: {
+      compress: true,
+      historyApiFallback: true,
+      open: true,
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+      },
+    },
+  };
+};
