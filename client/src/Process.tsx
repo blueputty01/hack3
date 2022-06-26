5b6955b3914837a8b3e27778f4156d4b2c2b78eb
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-26 10:50:51 -0400

    fix errors

--- client/src/Process.tsx
+++ client/src/Process.tsx
@@ -3,8 +3,8 @@ import Upload from './Upload';
 import api from './api';
 
 export default function Process() {
-  const a = new api();
   const [result, setResult] = useState('');
+  const a = new api();
   const responseHandler = async (data: FormData) => {
     const response = await a.getSalinity(data);
     setResult(response.data);
