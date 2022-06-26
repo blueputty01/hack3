cdef6f6c3e1b6d6816f51c22e50ff525821bfe7d
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-25 21:20:37 -0400

    refactor and responsive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import i from './classysalt.png';
 import Button from 'react-bootstrap/Button';
+import Header from './Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
@@ -9,41 +9,20 @@ export default function App() {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'column',
+        height: '100vh',
       }}
     >
-      <div>
-        <h1
-          style={{
-            paddingTop: 30,
-            paddingBottom: 0,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
+      <Header></Header>
+      <div style={{ paddingBottom: 25 }}>
+        <Button
+          onClick={() => {
+            console.log('This thing works');
           }}
-          className="display-6 text-6xl font-black"
+          variant="btn btn-outline-primary"
         >
-          Hi, welcome to
-        </h1>
-        <img src={i} alt="..." height={550} width={750}></img>
-        <div
-          style={{
-            paddingTop: 5,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <div style={{ paddingBottom: 25 }}>
-            <Button
-              onClick={() => {
-                console.log('This thing works');
-              }}
-              variant="btn btn-outline-primary"
-            >
-              Upload Image
-            </Button>
-          </div>
-        </div>
+          Upload Image
+        </Button>
       </div>
     </main>
   );
